237ddeda9999ef1176877a8ddcef952f571ef091
jugbeep <patrickbotkins@gmail.com>
2018-01-30 17:01:39 -0700

    finished sprint1

--- starter-code/back-end/controllers/artists.js
+++ starter-code/back-end/controllers/artists.js
@@ -1,5 +1,6 @@
 var db = require('../models');
 var Artist = db.models.Artist;
+var Song = db.models.Song;
 
 function index(req, res) {
   Artist.findAll().then(function(artists) {
@@ -8,8 +9,10 @@ function index(req, res) {
 }
 
 function show(req, res) {
-  Artist.findById(req.params.id)
-  .then(function(artist){
+  
+  Artist.findById(req.params.id, {include: Song}) 
+
+  .then(function(artist) {
     if(!artist) res.send("artist not found");
     //Artist.sing();
     //artist.shout();
